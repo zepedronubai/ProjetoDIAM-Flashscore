e4e1e5364418b18794e65285070e3e5f672bb567
zepedronubai <zepedro.noworks@gmail.com>
2024-04-22 18:21:33 +0100

    Versao1, django check
    Models adicionados, django project criado

---
+++ flashscore/flashscoreapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FlashscoreappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'flashscoreapp'
---
+++ flashscore/flashscoreapp/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+import datetime
+
+# Create your models here.
+
+class Nacionalidade(models.Model):
+    pais = models.CharField(max_length=200)
+    nacionalidadeNome = models.CharField(max_length=200)
+    bandeira = models.CharField(default="",max_length=1000)
+
+class Liga(models.Model):
+    nomeDaLiga = models.CharField(max_length=200)
+    nrMaxEquipasDaLiga = models.IntegerField(default=10)
+    logoDaLiga = models.CharField(default="",max_length=1000)
+    
+class Equipa(models.Model):
+    nomeDaEquipa = models.CharField(max_length=200)
+    sigla = models.CharField(max_length=10)
+    liga = models.ForeignKey(Liga, on_delete=models.CASCADE)
+    logoDaEquipa = models.CharField(default="",max_length=1000)
+
+class Jogador(models.Model):
+    nomeDoJogador = models.CharField(max_length=200)
+    nrDoJogador = models.IntegerField()
+    dataDeNascimento = models.DateTimeField()
+    nacionalidadedoJogador = models.ForeignKey(Nacionalidade, on_delete=models.CASCADE)
+    equipaDoJogador = models.ForeignKey(Equipa, on_delete=models.CASCADE)
+    fotoDoJogador = models.CharField(default="",max_length=1000)
+
