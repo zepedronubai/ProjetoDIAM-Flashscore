6f0b8dc235802d0d6893f373d9f042cfc625aa9b
zepedronubai <zepedro.noworks@gmail.com>
2024-04-22 19:11:43 +0100

    Versao 1 - Mostrar os models
    Teste dos models.
    Alguns criados.
    Todos mostrados

--- flashscore/flashscoreapp/views.py
+++ flashscore/flashscoreapp/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Liga,Equipa,Jogador,Nacionalidade
+
 
 # Create your views here.
 
 
 def index(request):
-    return render(request,"./index.html")
+    ligas = Liga.objects.all
+    print(ligas)
+    return render(request,"./index.html",{'ligas':ligas})
 
 
